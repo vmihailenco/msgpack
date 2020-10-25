abcfc8b5a2b7c0b744fe40cd10a2514c0fcdaa9e
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2020-10-25 11:04:34 +0200

    Extra appengine support to separate package

--- extra/msgpappengine/appengine.go
+++ extra/msgpappengine/appengine.go
@@ -1,31 +1,30 @@
-// +build appengine
-
-package msgpack
+package msgpappengine
 
 import (
 	"reflect"
 
+	"github.com/vmihailenco/msgpack/v5"
 	ds "google.golang.org/appengine/datastore"
 )
 
 func init() {
-	Register((*ds.Key)(nil), encodeDatastoreKeyValue, decodeDatastoreKeyValue)
-	Register((*ds.Cursor)(nil), encodeDatastoreCursorValue, decodeDatastoreCursorValue)
+	msgpack.Register((*ds.Key)(nil), encodeDatastoreKeyValue, decodeDatastoreKeyValue)
+	msgpack.Register((*ds.Cursor)(nil), encodeDatastoreCursorValue, decodeDatastoreCursorValue)
 }
 
-func EncodeDatastoreKey(e *Encoder, key *ds.Key) error {
+func EncodeDatastoreKey(e *msgpack.Encoder, key *ds.Key) error {
 	if key == nil {
 		return e.EncodeNil()
 	}
 	return e.EncodeString(key.Encode())
 }
 
-func encodeDatastoreKeyValue(e *Encoder, v reflect.Value) error {
+func encodeDatastoreKeyValue(e *msgpack.Encoder, v reflect.Value) error {
 	key := v.Interface().(*ds.Key)
 	return EncodeDatastoreKey(e, key)
 }
 
-func DecodeDatastoreKey(d *Decoder) (*ds.Key, error) {
+func DecodeDatastoreKey(d *msgpack.Decoder) (*ds.Key, error) {
 	v, err := d.DecodeString()
 	if err != nil {
 		return nil, err
@@ -36,7 +35,7 @@ func DecodeDatastoreKey(d *Decoder) (*ds.Key, error) {
 	return ds.DecodeKey(v)
 }
 
-func decodeDatastoreKeyValue(d *Decoder, v reflect.Value) error {
+func decodeDatastoreKeyValue(d *msgpack.Decoder, v reflect.Value) error {
 	key, err := DecodeDatastoreKey(d)
 	if err != nil {
 		return err
@@ -45,12 +44,12 @@ func decodeDatastoreKeyValue(d *Decoder, v reflect.Value) error {
 	return nil
 }
 
-func encodeDatastoreCursorValue(e *Encoder, v reflect.Value) error {
+func encodeDatastoreCursorValue(e *msgpack.Encoder, v reflect.Value) error {
 	cursor := v.Interface().(ds.Cursor)
 	return e.Encode(cursor.String())
 }
 
-func decodeDatastoreCursorValue(d *Decoder, v reflect.Value) error {
+func decodeDatastoreCursorValue(d *msgpack.Decoder, v reflect.Value) error {
 	s, err := d.DecodeString()
 	if err != nil {
 		return err
---
+++ extra/msgpappengine/go.mod
@@ -0,0 +1,8 @@
+module github.com/vmihailenco/msgpack/extra/appengine
+
+go 1.15
+
+require (
+	github.com/vmihailenco/msgpack/v5 v5.0.0-beta.5
+	google.golang.org/appengine v1.6.7
+)
